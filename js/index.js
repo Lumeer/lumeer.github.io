8c7c536d54a4ab8100c5d6fb3390ec4155c4f8ee
Martin Večeřa <marvenec@gmail.com>
2018-11-09 10:10:03 +0000

    Fix hamburger behaviour

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    document.querySelector('.mouse').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function ()
+{
+    let columns = document.querySelectorAll('.first-benefits .main-column');
+    document.querySelector('.mouse').addEventListener('click', function ()
+    {
         zenscroll.toY(document.querySelector('.home .upper-content').clientHeight - document.querySelector('header nav').clientHeight);
-    });
+    }, false);
 
     let blogGlide = new Glide('.blog .glide', {
         type: 'carousel',
@@ -19,27 +21,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
     checkMobileFunctions();
 
-    function checkMobileFunctions() {
-        for (let column of document.querySelectorAll('.first-benefits .main-column')) {
-            let text = column.querySelector('.text');
-
-            text.style.height = 'auto';
-
-            column.dataset.textHeight = text.offsetHeight;
-            text.style.visibility = 'visible';
-            text.style.height = '0';
-
-            column.addEventListener('click', function() {
-                let $text = column.querySelector('.text');
-
-                if (this.classList.contains('active')) {
-                    this.classList.remove('active');
-                    $text.style.height = '0';
-                } else {
-                    this.classList.add('active');
-                    $text.style.height = column.dataset.textHeight + 'px';
-                }
-            }, false);
+    window.addEventListener('resize', () =>
+    {
+        for (let column of columns)
+        {
+            checkMobileFunctions(column);
         }
+    });
+
+    function checkMobileFunctions(column)
+    {
+        let text = column.querySelector('.text');
+
+        text.style.height = 'auto';
+
+        if (window.innerWidth > 1023)
+        {
+            return;
+        }
+
+        column.dataset.textHeight = text.offsetHeight;
+        text.style.visibility = 'visible';
+        text.style.height = '0';
+    }
+
+    for (let column of document.querySelectorAll('.first-benefits .main-column'))
+    {
+        checkMobileFunctions(column);
+        column.addEventListener('click', function ()
+        {
+            let $text = column.querySelector('.text');
+
+            if (this.classList.contains('active'))
+            {
+                this.classList.remove('active');
+                $text.style.height = '0';
+            }
+            else
+            {
+                this.classList.add('active');
+                $text.style.height = column.dataset.textHeight + 'px';
+            }
+        }, false);
     }
 });
