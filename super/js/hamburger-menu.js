9578cba348e2a1966336cd075f1db9d26cc26376
Martin Večeřa <marvenec@gmail.com>
2020-02-21 17:01:08 +0100

    Implemented more components

--- super/js/hamburger-menu.js
+++ super/js/hamburger-menu.js
@@ -15,7 +15,39 @@ function toggleClass(elem, className) {
     }
 }
 
+function removeClass(elem, className) {
+    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+        elem.className += ' ' + className;
+    }
+}
+
 function hamburgerToggle() {
     toggleClass(document.getElementById('hamburger-menu'), 'open');
     toggleClass(document.getElementById('side-menu-mobile'), 'open');
+}
+
+function selectActiveTab(index, className, idPrefix, contentSuffix = '', max = 3) {
+    for (let i = 1; i <= max; i++) {
+        if (index != i) {
+            removeClass(document.getElementById(idPrefix + i), className);
+            if (contentSuffix) {
+                removeClass(document.getElementById(idPrefix + contentSuffix + i), className);
+            }
+        }
+    }
+    
+    addClass(document.getElementById(idPrefix + index), className);
+    if (contentSuffix) {
+        addClass(document.getElementById(idPrefix + contentSuffix + index), className);
+    }
 }
