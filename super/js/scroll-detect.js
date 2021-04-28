c78daaaec5d98159674a64a6967e080353d3f0ff
Martin Večeřa <marvenec@gmail.com>
2021-04-28 23:40:33 +0200

    Update blog font

--- super/js/scroll-detect.js
+++ super/js/scroll-detect.js
@@ -27,6 +27,14 @@ const debounce = (fn) => {
 // so we can use it in our stylesheets
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
+
+  var elems = [...document.getElementsByClassName('lumeer-page-scroll-depth-bar')];
+  elems.forEach(function(elem) {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    elem.style.width = scrolled + '%';
+  });
 }
 
 // Listen for new scroll events, here we debounce our `storeScroll` function
